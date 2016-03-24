4ae56913824898774c1b65126c79f8a6fe2c8f37
Les Pounder <lespounder@gmail.com>
2016-03-24 07:29:46 +0000

    Testing

---
+++ gpio-sonic-sampler.py
@@ -0,0 +1,30 @@
+from gpiozero import LED, Button
+from psonic import *
+#from signal import pause
+
+red = LED(5)
+yellow = LED(6)
+green = LED(13)
+
+button1 = Button(2)
+button2 = Button(3)
+button3 = Button(4)
+
+while True:
+    if button1.is_pressed:
+        print("Button 1 Pressed")
+        red.on()
+        play(c)
+    elif button2.is_pressed:
+        print("Button 2 is Pressed")
+        yellow.on()
+        play(g)
+    elif button3.is_pressed:
+        print("Button 3 is Pressed")
+        green.on()
+        play(d)
+    else:
+        red.off()
+        yellow.off()
+        green.off()
+        
