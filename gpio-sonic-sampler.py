19991815c8b3b525d820323daebed0ec27ba4a8b
Les Pounder <lespounder@gmail.com>
2016-03-24 12:36:50 +0000

    Updated the code to include a delay to stop the button bouncing

--- gpio-sonic-sampler.py
+++ gpio-sonic-sampler.py
@@ -1,6 +1,6 @@
 from gpiozero import LED, Button
 from psonic import *
-#from signal import pause
+import time
 
 red = LED(5)
 yellow = LED(6)
@@ -12,14 +12,17 @@ button3 = Button(4)
 
 while True:
     if button1.is_pressed:
+        time.sleep(0.1)
         print("Button 1 Pressed")
         red.on()
         play(c)
     elif button2.is_pressed:
+        time.sleep(0.1)
         print("Button 2 is Pressed")
         yellow.on()
         play(g)
     elif button3.is_pressed:
+        time.sleep(0.1)
         print("Button 3 is Pressed")
         green.on()
         play(d)
