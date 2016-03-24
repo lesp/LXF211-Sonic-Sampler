3d3b35bf756609c051e80ad231e1325225624d5e
Les Pounder <lespounder@gmail.com>
2016-03-24 19:30:24 +0000

    Updated

--- gpio-sonic-sampler.py
+++ gpio-sonic-sampler.py
@@ -1,6 +1,7 @@
 from gpiozero import LED, Button
 from psonic import *
 import time
+from random import uniform
 
 red = LED(5)
 yellow = LED(6)
@@ -12,20 +13,20 @@ button3 = Button(4)
 
 while True:
     if button1.is_pressed:
-        time.sleep(0.1)
+        time.sleep(0.3)
         print("Button 1 Pressed")
         red.on()
-        play(c)
+        sample(LOOP_AMEN, rate=uniform(0.5,2))
     elif button2.is_pressed:
         time.sleep(0.1)
         print("Button 2 is Pressed")
         yellow.on()
-        play(g)
+        play(G5)
     elif button3.is_pressed:
         time.sleep(0.1)
         print("Button 3 is Pressed")
         green.on()
-        play(d)
+        play(D5)
     else:
         red.off()
         yellow.off()
